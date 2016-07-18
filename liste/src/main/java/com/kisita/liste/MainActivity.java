7117aeedf53ac7f776ec02c0968d76db78526da8
Hugues Kisita <Hugues Kisita>
2016-07-18 21:53:07 +0200

    Add sample related to lists

--- liste/src/main/java/com/kisita/liste/MainActivity.java
+++ liste/src/main/java/com/kisita/liste/MainActivity.java
@@ -1,17 +1,42 @@
 package com.kisita.liste;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends ListActivity {
+    //private TextView selection;
+    private static ListView myList;
+    private static final String[] items = {
+            "Tresses",
+            "Rasta",
+            "Coupe",
+            "Coiffure évenements",
+            "Defrisage",
+            "Soins capillaires"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice, items));
+        //selection=(TextView)findViewById(R.id.selection);
+        myList=(ListView)findViewById(R.id.
+
+
     }
+
+  /*  public void onListItemClick(ListView parent, View v,int position, long id)
+    {
+        selection.setText(items[position]);
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
